25541ea5c2c9576b23526e2359ddf1e9c5a33ee1
ThimBoer <tboer@snelstart.nl>
2024-01-17 15:39:12 +0100

    added unit-tests

---
+++ src/test/java/recipeapplication/application/TokenTests/TokenTests.java
@@ -0,0 +1,51 @@
+package recipeapplication.application.TokenTests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import recipeapplication.application.models.Role;
+import recipeapplication.application.models.User;
+import recipeapplication.application.services.TokenService;
+
+@SpringBootTest
+class TokenTests {
+    @Test
+    public void testGenerateTokenForUser() {
+        TokenService tokenService = mock(TokenService.class);
+
+        User user = new User();
+        user.setFirstname("user");
+        user.setLastname("user");
+        user.setPassword("password");
+        user.setRole(Role.VIEWER);
+
+        String mockToken = "mockedToken";
+        when(tokenService.generateToken(user)).thenReturn(mockToken);
+
+        String generatedToken = tokenService.generateToken(user);
+
+        assertEquals(mockToken, generatedToken, "Het gegenereerde token komt niet overeen met het verwachte token.");
+    }
+
+    @Test
+    public void testGenerateRefreshTokenForUser() {
+        TokenService tokenService = mock(TokenService.class);
+
+        User user = new User();
+        user.setFirstname("user");
+        user.setLastname("user");
+        user.setPassword("password");
+        user.setRole(Role.VIEWER);
+        
+        String mockToken = "mockedToken";
+        when(tokenService.generateRefreshToken(user)).thenReturn(mockToken);
+
+        String generatedToken = tokenService.generateRefreshToken(user);
+
+        assertEquals(mockToken, generatedToken, "Het gegenereerde token komt niet overeen met het verwachte token.");
+    }
+}
